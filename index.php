bda4990793fe40e1a4f1bdc8dedccb97285ce8a1
nerun <danieldiasr@gmail.com>
2023-12-18 10:33:01 -0300

    Code revision and a few improvements.

--- index.php
+++ index.php
@@ -16,43 +16,45 @@ if ($_GET) {
 
 if ( $page_file != $main ) {
     $body = file_get_contents($page_file);
-}
-
-if ( !empty($body) ) {
-    $parameters = _getParams($body, $page_file);
-    $article = $parameters[0];
-    $author  = $parameters[1];
-    $columns = $parameters[2];
-    $date    = $parameters[3];
-    $email   = $parameters[4];
-    $image   = $parameters[5];
     
-    // If email is not missing, link it to the author
-    if ( !empty($email) ){
-        $author = '<a href="mailto:'.$email.'" target="_blank" rel="noopener noreferrer">'.$author.'</a>';
-    }
+    if ( !empty($body) ) {
+        $parameters = _getParams($body, $page_file);
+        $article = $parameters[0];
+        $author  = $parameters[1];
+        $columns = $parameters[2];
+        $date    = $parameters[3];
+        $email   = $parameters[4];
+        $image   = $parameters[5];
+        
+        // If email is not missing, link it to the author
+        if ( !empty($email) ){
+            $author = '<a href="mailto:'.$email.'">'.$author.'</a>';
+        }
 
-    // If featured image is not missing and it is not set to 'none', insert it
-    if ( !empty($image) && mb_strtolower($image) != "none" ){
-        echo '<img src="/img/'.$image.'" width="640" height="360" style="margin: 1px auto 1px; display: block;">';
-        echo '<hr width="75%">';
-    }
+        // If featured image is not missing and it is not set to 'none', insert it
+        if ( !empty($image) && mb_strtolower($image) != "none" ){
+            echo '<img src="/img/'.$image.'" width="640" height="360"
+                 style="margin: 1px auto 1px; display: block;">';
+            echo '<hr width="75%">';
+        }
 
-    // Insert title, author and publication date in the page, using Unicode characters as icons.
-    echo '<h1 style="margin-bottom:0; text-align:center;">'.$article.'</h1>';
-    echo '<p style="margin-top:0; font-size:75%; text-align:center;">&#128100; '.$author.'&emsp;&#128197; '.$date.'</p>';
-    echo '<br />';
-    echo '<div id="columns" class="columns" style="column-count:'.$columns.';">';
-    
-    if ( mb_strtolower(substr($page_file, -2)) == 'md' ) {
-        echo $Parsedown->text($body);
+        // Insert title, author and publication date in the page, using Unicode characters as icons.
+        echo '<h1 style="margin-bottom:0; text-align:center;">'.$article.'</h1>';
+        echo '<p style="margin-top:0; font-size:75%; text-align:center;">&#128100; '
+             .$author.'&emsp;&#128197; '.$date.'</p>';
+        echo '<br />';
+        echo '<div id="columns" class="columns" style="column-count:'.$columns.';">';
+        
+        if ( mb_strtolower(substr($page_file, -2)) == 'md' ) {
+            echo $Parsedown->text($body);
+        } else {
+            echo $body;
+        }
     } else {
-        echo $body;
+        include('404.php');
     }
-} elseif ( $page_file == $main ) {
-    include($main);
 } else {
-    include("404.php");
+    include($main);
 }
 
 $footer = file_get_contents("footer.html");
@@ -66,11 +68,11 @@ function _getParams($page, $page_name) {
     // Alternatively, you can change the default missing string to set a
     // default author, email etc.
     $art  = _getVar('arti(?:cle|go)', $page, 'article title missing');
-    $aut  = _getVar('auth?or', $page, 'author name missing');
-    $col  = _getVar('colu(?:mn|na)s?', $page, 'auto');
+    $aut  = _getVar('auth?or', $page, 'Daniel Dias Rodrigues');
+    $col  = _getVar('colu(?:mn|na)s?', $page, '2');
     // for date(), refer to https://www.php.net/manual/en/function.date.php
     $dat  = _getVar('dat[ae]', $page, date(DATE_RFC2822, filemtime($page_name)));
-    $mail = _getVar('e?mail', $page, null);
+    $mail = _getVar('e?mail', $page, 'gurpzine@gurpzine.com.br');
     $img  = _getVar('imagem?', $page, 'none');
     
     return array( $art, $aut, $col, $dat, $mail, $img );
--- main.php
+++ main.php
@@ -13,21 +13,23 @@ $ignored = array('.', '..');
 $dirs = array_diff( $dirs, $ignored );
 $dirs = array_values( $dirs );
 
-echo '<style>';
-echo 'a { color: #2467ab; text-decoration: none; };';
-echo 'a:hover { text-decoration: underline; };';
-echo '</style>';
+echo '<div class="abstract">';
 
 foreach ( $dirs as $key => $value ) {
-    // Extract the first 600 characters from each file.
-    $briefing = file_get_contents("articles/$value", false, null, 0, 600);
-
-     /**
-     * Getting the parameters is only possible if the parameters section
-     * is at the top of the file. So make sure you always position it at
-     * the top.
-     */    
-    $parameters = _getParams($briefing, "articles/$value");
+    // Open file as array
+    $abstract = file("articles/$value");
+    // Extract the first 11 lines from each file.
+    $abstract = implode(array_slice($abstract, 0, 11));
+    // No images beyond featured ones, and no headings in the abstract.
+    $abstract = preg_replace('/(.*)<img src=\"[^"]*\".*\/>(.*)/s', '\1\2', $abstract);
+    $abstract = preg_replace('/(.*)<h.*>.*<\/h.>(.*)/s', '\1\2', $abstract);
+    
+    /**
+    * Getting the parameters is only possible if the parameters section
+    * is at the top of the file. So make sure you always position it at
+    * the top.
+    */    
+    $parameters = _getParams($abstract, "articles/$value");
     $article = $parameters[0];
     $author  = $parameters[1];
     $columns = $parameters[2];
@@ -36,20 +38,33 @@ foreach ( $dirs as $key => $value ) {
     $image   = $parameters[5];
     
     if ( !empty($image) && mb_strtolower($image) != "none" ){
-        echo '<a href="articles/'.$value.'"><img src="/img/'.$image.'" width="432" height="216" style="float: right; padding-left: 15px"></a>';
+        echo '<a href="articles/'.$value.'" target="_top"><img src="/img/'.$image.'"
+             width="340" height="170" style="float:right; margin-left:15px;
+             margin-bottom:15px"></a>';
     }
     
-    echo '<h1 style="margin-top:0; margin-bottom:0; text-align:left;"><a href="articles/'.$value.'">'.$article.'</a></h1>';
-    echo '<p style="margin-top:0; font-size:75%;">&#128100; '.$author.'&emsp;&#128197; '.$date.'</p>';
+    echo '<h1 style="margin-top:0; margin-bottom:0; text-align:left;">
+         <a href="articles/'.$value.'" target="_top">'.$article.'</a></h1>';
+    
+    echo '<p style="margin-top:0; font-size:75%;">&#128100; '.$author.
+         '&emsp;&#128197; '.$date.'</p>';
+    
+    $readmore = '<a href="articles/'.$value.'" target="_top">
+            <img src="/img/readmore.png" height="24" style="vertical-align:top;" />
+            </a>';
     
     if ( mb_strtolower(substr($value, -2)) == 'md' ) {
-        echo $Parsedown->text($briefing . '... <a href="articles/' . $value . '">[read more]</a>');
+        echo $Parsedown->text($abstract);
     } else {
-        echo $briefing . '... <a href="articles/' . $value . '">[read more]</a>';
+        echo $abstract;
     }
+    
+    echo $readmore;
     
     if ( $dirs[$key] != end($dirs) ) {
         echo '<hr>';
     }
 }
+
+echo '</div>';
 ?>
