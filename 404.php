4f5fb671c1bf9a120910bb93a53dd8bbd5c91bf9
nerun <danieldiasr@gmail.com>
2025-02-03 13:52:26 -0300

    page 404 formatting

--- 404.php
+++ 404.php
@@ -1,18 +1,18 @@
 <?php
-echo '<h1 style="line-height:5px; text-align:center;">Oops!</h1>';
+echo tab(3) . '<h1 style="line-height:5px; text-align:center;">Oops!</h1>' . "\n";
 
-echo '<h2 style="text-align:center;">Error 404. Page not found.</h2>';
+echo tab(3) . '<h2 style="text-align:center;">Error 404. Page not found.</h2>' . "\n";
 
-echo '<blockquote>';
+echo tab(3) . "<blockquote>\n";
 
-echo '<hr>';
+echo tab(4) . "<hr>\n";
 
-echo '<p>In computer network communications, the <b>HTTP 404</b>, <b>404 not found</b>, <b>404</b>, <b>404 error</b>, <b>page not found</b>, or <b>file not found</b> error message is a hypertext transfer protocol (HTTP) standard response code, to indicate that the browser was able to communicate with a given server, but the server could not find what was requested. The error may also be used when a server does not wish to disclose whether it has the requested information.</p>';
+echo tab(4) . '<p>In computer network communications, the <b>HTTP 404</b>, <b>404 not found</b>, <b>404</b>, <b>404 error</b>, <b>page not found</b>, or <b>file not found</b> error message is a hypertext transfer protocol (HTTP) standard response code, to indicate that the browser was able to communicate with a given server, but the server could not find what was requested. The error may also be used when a server does not wish to disclose whether it has the requested information.</p>' . "\n";
 
-echo '<p>The website hosting server will typically generate a "404 Not Found" web page when a user attempts to follow a broken or dead link; hence the 404 error is one of the most recognizable errors encountered on the World Wide Web.</p>';
+echo tab(4) . '<p>The website hosting server will typically generate a "404 Not Found" web page when a user attempts to follow a broken or dead link; hence the 404 error is one of the most recognizable errors encountered on the World Wide Web.</p>' . "\n";
 
-echo '<p style="text-align:right;">&mdash; Wikipedia: <a href="https://en.wikipedia.org/wiki/HTTP_404">HTTP 404</p>';
+echo tab(4) . '<p style="text-align:right;">&mdash; Wikipedia: <a href="https://en.wikipedia.org/wiki/HTTP_404">HTTP 404</a></p>' . "\n";
 
-echo '<hr>';
-echo '</blockquote>';
+echo tab(4) . "<hr>\n";
+echo tab(3) . "</blockquote>\n";
 ?>
